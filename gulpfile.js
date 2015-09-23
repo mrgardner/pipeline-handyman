0f6b077facf71676eb61893dda9dbdd95b1ac6cc
Owen Buckley <obuckley@kenzan.com>
2015-09-23 10:38:27 -0400

    brute force reset of master

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-require('pipeline-validate-js')(gulp);
-require('./src/index.js');
-
-//Validates pipeline files.
-gulp.task('default', ['pipelineValidateJS']);
